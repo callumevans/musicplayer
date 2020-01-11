dc7981d5d86e42c7abd977e5187e46c20a1daddd
Callum Evans <callumevans@outlook.com>
2020-01-11 14:18:09 +0000

    PWA manifest

--- src/index.js
+++ src/index.js
@@ -6,4 +6,14 @@ const app = new App({
     },
 });
 
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js')
+        .then(function(registration) {
+            console.log('Registration successful, scope is:', registration.scope);
+        })
+        .catch(function(error) {
+            console.log('Service worker registration failed, error:', error);
+        });
+}
+
 export default app;
